845798325adcae814ce2bcd9dd518f6322ba68d1
Matteo <matteo.grisenti@studenti.unitn.it>
2025-03-29 17:20:35 +0100

    Setup codice: copia-incolla codice di partenza

---
+++ LM/A/functions.py
@@ -0,0 +1,95 @@
+import math
+import torch
+import torch.nn as nn
+
+
+def train_loop(data, optimizer, criterion, model, clip=5):
+    # data: training datatest containing samples. Each sample is a dictionary with:
+    #       - "source": a sequence of IDs representing the input.
+    #       - "target": a sequence of IDs representing the output.
+    #       - "number_tokens": the number of tokens in the sequence.
+    # optimizer: optimizer to update the weights of the model.
+    # criterion: loss function to compute the loss.
+    # model: the model to be trained.
+    # clip: gradient clipping value to avoid exploding gradients.
+
+    model.train()
+    loss_array = []
+    number_of_tokens = []
+    
+    for sample in data:
+        optimizer.zero_grad()               # Zeroing the gradient
+        output = model(sample['source'])    # Forward pass: compute predicted outputs by passing inputs to the model
+        
+        loss = criterion(output, sample['target'])                  # Compute the loss
+        loss_array.append(loss.item() * sample["number_tokens"])
+        number_of_tokens.append(sample["number_tokens"])
+
+        loss.backward() # Compute the gradient, deleting the computational graph
+        # clip the gradient to avoid explosioning gradients
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)  
+
+        optimizer.step() # Update the weights
+    
+    # return the average loss over the batch
+    return sum(loss_array)/sum(number_of_tokens)
+
+
+
+
+def eval_loop(data, eval_criterion, model):
+    # data: training datatest containing samples. Each sample is a dictionary with:
+    #       - "source": a sequence of IDs representing the input.
+    #       - "target": a sequence of IDs representing the output.
+    #       - "number_tokens": the number of tokens in the sequence.
+    # eval_criterion: loss function to compute the evaluation.
+    # model: the model to be evaluated.
+
+    model.eval()
+    loss_to_return = []
+    loss_array = []
+    number_of_tokens = []
+
+    # softmax = nn.Softmax(dim=1) # Use Softmax if you need the actual probability
+    with torch.no_grad(): # It used to avoid the creation of computational graph
+        for sample in data:
+            output = model(sample['source'])
+            loss = eval_criterion(output, sample['target'])
+            loss_array.append(loss.item())
+            number_of_tokens.append(sample["number_tokens"])
+            
+    ppl = math.exp(sum(loss_array) / sum(number_of_tokens))
+    loss_to_return = sum(loss_array) / sum(number_of_tokens)
+
+    return ppl, loss_to_return
+
+
+
+
+def init_weights(mat):
+    for m in mat.modules():
+        if type(m) in [nn.GRU, nn.LSTM, nn.RNN]:
+            for name, param in m.named_parameters():
+                if 'weight_ih' in name:
+                    for idx in range(4):
+                        mul = param.shape[0]//4
+                        torch.nn.init.xavier_uniform_(param[idx*mul:(idx+1)*mul])
+                elif 'weight_hh' in name:
+                    for idx in range(4):
+                        mul = param.shape[0]//4
+                        torch.nn.init.orthogonal_(param[idx*mul:(idx+1)*mul])
+                elif 'bias' in name:
+                    param.data.fill_(0)
+        else:
+            if type(m) in [nn.Linear]:
+                torch.nn.init.uniform_(m.weight, -0.01, 0.01)
+                if m.bias != None:
+                    m.bias.data.fill_(0.01)
+
+
+
+
+def save_model(model):
+    path = 'bin/model.pt'
+    torch.save(model.state_dict(), path)
+
---
+++ LM/A/main.py
@@ -0,0 +1,97 @@
+import math
+import copy
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from torch import optim
+from functools import partial
+from torch.utils.data import DataLoader
+
+
+from utils import read_file, Lang, PennTreeBank, collate_fn
+from model import LM_RNN
+from functions import init_weights, train_loop, eval_loop
+
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+train_raw = read_file("dataset/PennTreeBank/ptb.train.txt")
+dev_raw = read_file("dataset/PennTreeBank/ptb.valid.txt")
+test_raw = read_file("dataset/PennTreeBank/ptb.test.txt")
+
+lang = Lang(train_raw, ["<pad>", "<eos>"])
+# print(len(lang.word2id))
+
+train_dataset = PennTreeBank(train_raw, lang)
+dev_dataset   = PennTreeBank(dev_raw, lang)
+test_dataset  = PennTreeBank(test_raw, lang)
+
+# print("Train dataset size: ", len(train_dataset))
+# print("Dev dataset size: ", len(dev_dataset))
+# print("Test dataset size: ", len(test_dataset))
+
+# Dataloader instantiation
+# NB: You can reduce the batch_size if the GPU memory is not enough
+train_loader = DataLoader(train_dataset, batch_size=64,  collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"], DEVICE=DEVICE),  shuffle=True)
+dev_loader   = DataLoader(dev_dataset,   batch_size=128, collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"], DEVICE=DEVICE))
+test_loader  = DataLoader(test_dataset,  batch_size=128, collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"], DEVICE=DEVICE))
+
+
+# Model configuration 
+hid_size = 200
+emb_size = 300
+lr = 0.0001 # This is definitely not good for SGD
+# NB: With SGD try with an higher learning rate (> 1 for instance)
+clip = 5 # Clip the gradient
+
+
+# Experiment also with a smaller or bigger model by changing hid and emb sizes 
+# NB: A large model tends to overfit
+# Don't forget to experiment with a lower training batch size
+# Increasing the back propagation steps can be seen as a regularization step
+
+
+vocab_len = len(lang.word2id)
+
+model = LM_RNN(emb_size, hid_size, vocab_len, pad_index=lang.word2id["<pad>"]).to(DEVICE)
+model.apply(init_weights)
+
+optimizer = optim.SGD(model.parameters(), lr=lr)
+criterion_train = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"])
+criterion_eval = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"], reduction='sum')
+
+
+n_epochs = 100
+patience = 3
+losses_train = []
+losses_dev = []
+sampled_epochs = []
+best_ppl = math.inf
+best_model = None
+pbar = tqdm(range(1,n_epochs))
+
+#If the PPL is too high try to change the learning rate
+for epoch in pbar:
+    loss = train_loop(train_loader, optimizer, criterion_train, model, clip)    
+    if epoch % 1 == 0:
+        sampled_epochs.append(epoch)
+        losses_train.append(np.asarray(loss).mean())
+        ppl_dev, loss_dev = eval_loop(dev_loader, criterion_eval, model)
+        losses_dev.append(np.asarray(loss_dev).mean())
+        pbar.set_description("PPL: %f" % ppl_dev)
+        if  ppl_dev < best_ppl: # the lower, the better
+            best_ppl = ppl_dev
+            best_model = copy.deepcopy(model).to('cpu')
+            patience = 3
+        else:
+            patience -= 1
+            
+        if patience <= 0: # Early stopping with patience
+            break # Not nice but it keeps the code clean
+
+best_model.to(DEVICE)
+final_ppl,  _ = eval_loop(test_loader, criterion_eval, best_model)    
+print('Test ppl: ', final_ppl)
