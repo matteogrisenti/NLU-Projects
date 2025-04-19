7f4cd39191a797c3b9bba26982a69c7435360460
matteogrisenti02 <grisentimatteo02@gmail.com>
2025-04-19 16:42:42 +0000

    Replot LSTM

--- LM/A/main.py
+++ LM/A/main.py
@@ -28,29 +28,30 @@ print("\tTest dataset size: ", len(test_dataset))
 
 # --------------------------------------------  HYPERPARAMETERS ------------------------------------------------
 LABEL = 'LSTM'      # RNN, LSTM
-BATCH_SIZE = 128     # Original 64
+BATCH_SIZE = [16,32,64,128]     # Original 64
 HID_SIZE = 200      # Original 200
 EMB_SIZE = 300      # Original 300
 DROPOUT_EMB = None
 DROPOUT_OUT = None
-LR = [2, 1, 0.5]
+LR = 1
 OPTIMIZER = 'SGD'   # SGD or Adam
 CLIP = 5            # Clip the gradient -> avoid exploding gradients
 
 
 
 # -------------------------------------------- TRAINING ------------------------------------------------
-for lr in LR:
-    print("Training with learning rate: ", lr)
+for batchsize in BATCH_SIZE:
+    #print("Training with learning rate: ", lr)
+    print("Training with batch size: ", batchsize)
     train_model(
         train_dataset,
         dev_dataset,
         test_dataset,
         lang,
-        BATCH_SIZE=BATCH_SIZE,
+        BATCH_SIZE=batchsize,
         HID_SIZE=HID_SIZE,
         EMB_SIZE=EMB_SIZE,
-        LR=lr,
+        LR=LR,
         DROPOUT_EMB=DROPOUT_EMB,
         DROPOUT_OUT=DROPOUT_OUT,
         CLIP=CLIP,
